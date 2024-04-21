25ee0aff0ce1f57ebeb1aec1bb9834102216fef7
Akshay Singh <akshay03.singh03@gmail.com>
2024-04-21 11:17:07 +0530

    global exception handler for post api has created

--- src/main/java/com/blog/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/blog/Exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.blog.Exceptions;
 import com.blog.DTOs.ApiResponse;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -20,5 +19,11 @@ public class GlobalExceptionHandler extends RuntimeException{
     public ResponseEntity<String> categoryNotFoundExceptionHandler(CategoryNotFoundException categoryNotFoundException){
         String message=categoryNotFoundException.getMessage();
         return new ResponseEntity<String>(message, NOT_FOUND);
+    }
+
+    @ExceptionHandler(PostNotFoundException.class)
+    public ResponseEntity<String> postNotFoundExceptionHandler(PostNotFoundException postNotFoundException){
+        String message=postNotFoundException.getMessage();
+        return new ResponseEntity<>(message, NOT_FOUND);
     }
 }
