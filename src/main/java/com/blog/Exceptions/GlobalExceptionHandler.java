27e3749c3a022f60c07b6b11ac358181d271f92d
Akshay Singh <akshay03.singh03@gmail.com>
2024-04-22 13:26:19 +0530

    ----

--- src/main/java/com/blog/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/blog/Exceptions/GlobalExceptionHandler.java
@@ -9,16 +9,16 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 @RestControllerAdvice
 public class GlobalExceptionHandler extends RuntimeException{
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(UserNotFoundException userNotFoundException){
+    public ResponseEntity<ApiResponse> userNotFoundExceptionHandler(UserNotFoundException userNotFoundException){
         String message=userNotFoundException.getMessage();
         ApiResponse apiResponse=new ApiResponse(message,false);
-        return new ResponseEntity<ApiResponse>(apiResponse,NOT_FOUND);
+        return new ResponseEntity<>(apiResponse,NOT_FOUND);
     }
 
     @ExceptionHandler(CategoryNotFoundException.class)
     public ResponseEntity<String> categoryNotFoundExceptionHandler(CategoryNotFoundException categoryNotFoundException){
         String message=categoryNotFoundException.getMessage();
-        return new ResponseEntity<String>(message, NOT_FOUND);
+        return new ResponseEntity<>(message, NOT_FOUND);
     }
 
     @ExceptionHandler(PostNotFoundException.class)
